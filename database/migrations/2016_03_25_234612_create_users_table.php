a12c8bc4bdd9e367b924a1fb6af8eceac572d6e1
肖宇飞 <1328353143@qq.com>
2016-03-27 12:55:04 +0800

    user

--- database/migrations/2016_03_25_234612_create_users_table.php
+++ database/migrations/2016_03_25_234612_create_users_table.php
@@ -14,10 +14,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +25,13 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::create('users', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('email');
+            $table->text('body');
+            $table->timestamps();
+            $table->timestamp('published_at');
+        });
     }
 }
